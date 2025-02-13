cd46bd4a39c6a57e28417310ef9f01c650ec8417
swatmarek <67947219+swatmarek@users.noreply.github.com>
2025-02-13 08:29:51 +0100

    Update upmenu.js

--- upmenu.js
+++ upmenu.js
@@ -1,1 +1,1 @@
-document.querySelector('body').innerHTML = "<img src=https://swatmarek.github.io/test/login.jpg>";
+document.querySelector('body').innerHTML = "<img src=https://swatmarek.github.io/test/login.jpg style="width: 100%; height: auto;">";
